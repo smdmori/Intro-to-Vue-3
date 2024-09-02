899e39b8c855abb67e70f047bc6db29673213ab4
Mori <mortezasamadi1371@gmail.com>
2024-09-02 17:20:39 +0330

    code challenge 2: add image and link

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'جوراب',
+            description: 'این جوراب پشمی باعث میشه پشماشون بریزه',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://github.com/smdmori',
         }
     }
 })
