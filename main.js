ce363c3dc689dcc51f9c6580c956f7e29b26c7e9
Mori <mortezasamadi1371@gmail.com>
2024-09-02 19:40:25 +0330

    add color selection

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2223, color: "green"},
+                {id: 2224, color: "blue"},
+            ],
+            selectedColor: 'Not selected'
         }
     }
 })
