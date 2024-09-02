9b0bf7fd2b609870c275129e52261282c9740ad7
Mori <mortezasamadi1371@gmail.com>
2024-09-02 20:09:06 +0330

    add sizes list

--- main.js
+++ main.js
@@ -6,10 +6,16 @@ const app = Vue.createApp({
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2223, color: "green"},
-                {id: 2224, color: "blue"},
+                { id: 2223, color: "green"},
+                { id: 2224, color: "blue" },
             ],
-            selectedColor: 'Not selected'
+            selectedColor: 'Not selected',
+            sizes: [
+                { id: 0, value: 'S'},
+                { id: 1, value: 'M'},
+                { id: 2, value: 'L'},
+                { id: 3, value: 'XL'},
+            ]
         }
     }
 })
